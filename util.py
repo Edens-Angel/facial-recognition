fb47c979259fbd4b7f216a6043e27aa2cf1cf5e3
Paul Al-Dabagh <paul.al-dabagh@hva.nl>
2022-06-28 00:24:02 +0200

    added an base case for the sort util function

--- util.py
+++ util.py
@@ -1,13 +1,16 @@
 import cv2
 import os
 
-def draw_rectangle(img, x, y, w, h, rgb_color = (255, 0, 0)):
+"""
+Draws a rectangle around a ROI
+"""
+def draw_rectangle(img, x, y, w, h, rgb_color = (255, 0, 0), stroke_size = 2):
     if (x == 0 and y == 0):
         return
     
     rectangle_width = x + w
     rectangle_height = y + h
-    cv2.rectangle(img, (x, y), (rectangle_width, rectangle_height), rgb_color, 2)
+    cv2.rectangle(img, (x, y), (rectangle_width, rectangle_height), rgb_color, stroke_size)
 
 """
 Renames all files in a folder as their numberic index in the folder
@@ -15,6 +18,9 @@ returns new directory structure
 """
 def numberic_reorder_dir(dir_path):
     files = os.listdir(dir_path)
+    
+    if files[0].startswith('1') and files[-1].startswith(str(len(files))):
+        return files
     
     for k, file in enumerate(files):
         abs_path = os.path.abspath(dir_path)
